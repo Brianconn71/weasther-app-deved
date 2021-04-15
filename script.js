03af46b14539c9d962b468cd01ffbe7a5873187d
Brian Connolly <Brian.connolly71@gmail.com>
2021-04-15 12:58:43 +0100

    Added position coordinates to the javascript to find a users position to get the weather

--- script.js
+++ script.js
@@ -2,5 +2,14 @@
 
 // after page loads we get the location
 window.addEventListener('load', ()=> {
+    let long;
+    let lat;
 
+    //if the user has location services allowed then we find them with the users lat and longitude, pop up on browser
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position =>{
+            long = position.coords.longitude
+            lat = position.coords.latitude
+        });
+    }
 });
