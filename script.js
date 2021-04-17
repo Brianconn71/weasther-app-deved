b4017ace29f2744baf8a70be7df967aeb35000a5
Brian Connolly <Brian.connolly71@gmail.com>
2021-04-17 12:59:00 +0100

    Got the api to work in the console

--- script.js
+++ script.js
@@ -4,12 +4,34 @@
 window.addEventListener('load', ()=> {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description')
+    let temperatureDegree = document.querySelector('.temperature-degree')
+    let locationTimezone = document.querySelector('.location-timezone')
 
     //if the user has location services allowed then we find them with the users lat and longitude, pop up on browser
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
-            long = position.coords.longitude
-            lat = position.coords.latitude
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = 'https://corsanywhere.herokuapp.com/';
+
+            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=73d94735253c37eef5165eb09f430c33`;
+
+            // gets information from server
+            fetch(api)
+            //then we can do something with the data
+                .then(response =>{
+                    return response.json()
+                })
+                .then(data => {
+                    console.log(data)
+                    // {} cool way to shorten syntax and pull out all info from an array
+                    const {description} = data.weather;
+                    const {temp} = data.main;
+                    //set dom elements
+                })
         });
+        
     }
 });
